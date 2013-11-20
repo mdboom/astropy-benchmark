7571d329415c14a720eb771b40348205b05bd844
Michael Droettboom <mdboom@gmail.com>
2013-11-20 13:33:32 -0500

    Update versions

--- benchmarks/test_coordinates.py
+++ benchmarks/test_coordinates.py
@@ -1,8 +1,8 @@
 import unittest
 
 from astropy import coordinates
-
+from astropy import units
 
 class TestLatitude(unittest.TestCase):
     def test_latitude(self):
-        coordinates.Latitude(3.2)
+        coordinates.Latitude(3.2, units.degree)
