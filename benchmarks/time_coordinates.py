e4992fb88e38c4e23adab30fac5468fedb1b4231
Michael Droettboom <mdboom@gmail.com>
2013-12-12 12:06:35 -0500

    Updated for new benchmark API

--- benchmarks/time_coordinates.py
+++ benchmarks/time_coordinates.py
@@ -1,8 +1,6 @@
-import unittest
-
 from astropy import coordinates
 from astropy import units
 
-class TestLatitude(unittest.TestCase):
-    def test_latitude(self):
-        coordinates.Latitude(3.2, units.degree)
+
+def time_latitude():
+    coordinates.Latitude(3.2, units.degree)
--- benchmarks/time_units.py
+++ benchmarks/time_units.py
@@ -1,11 +1,17 @@
-import unittest
-
 from astropy import units as u
 
 
-class TestUnit(unittest.TestCase):
-    def test_unit_compose(self):
+class TimeUnit:
+    def time_unit_compose(self):
         u.Ry.compose()
 
-    def test_unit_to(self):
+    def time_unit_to(self):
         u.m.to(u.pc)
+
+
+def time_unit_parse():
+    u.Unit('1e-07 kg m2 / s2')
+
+
+def mem_unit():
+    return u.erg
