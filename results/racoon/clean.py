dfa83480651bdabf69d3886148c8b859a7a45d20
Michael Droettboom <mdboom@gmail.com>
2013-12-13 14:32:35 -0500

    Remove OS-X tests, trying something new

--- results/racoon/clean.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-for file in os.listdir('.'):
-    if file.endswith('.json'):
-        d = json.load(open(file, 'rb'))
-        if 'version' not in d:
-            print(file)
-            os.remove(file)
